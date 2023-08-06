b4c6d729e36b5fe1dbfa9b206447c3eebe543593
RolandSzentesi <rampageroli@gmail.com>
2023-08-06 10:11:14 -0500

    private method update

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,8 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create({name: params[:name], species: params[:species]})
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +21,16 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+  # Using a private method to encapsulate the permissible parameters
+    # is just a good pattern since you'll be able to reuse the same
+    # permit list between create and update. Also, you can specialize
+    # this method with per-user checking of permissible attributes.
+    
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
